901065fe370bc286d4103e47f9a5c98d53255241
Frog237 <yt86tu@mail.ru>
2025-03-04 23:24:40 +0300

    Удалил лишнее

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Dog extends Animal {
-
-    private Timer timer = new Timer();
-
     @Override
     public String toString() { return "Im a Dog"; }
 }
